9a154d35f78ddd6a8f9dd9c7a53cbd82d3cb2764
Dennis Flannigan <DLFlann@gmail.com>
2017-09-11 22:11:34 -0500

    Add link to allow logged in users to submit course ideas to be added to a list and displayed

--- src/main/java/com/dlflann/spark/Main.java
+++ src/main/java/com/dlflann/spark/Main.java
@@ -1,5 +1,6 @@
 package com.dlflann.spark;
 
+import com.dlflann.spark.model.CourseIdea;
 import com.dlflann.spark.model.CourseIdeaDAO;
 import com.dlflann.spark.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
@@ -8,9 +9,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class Main
 {
@@ -37,6 +36,16 @@ public class Main
             Map<String, Object> model = new HashMap<>();
             model.put("ideas", dao.findAll());
             return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            String title = req.queryParams("title");
+            // TODO:dlf - This username is tied to the cookie implementation
+            CourseIdea courseIdea = new CourseIdea(title, req.cookie
+                    ("username"));
+            dao.add(courseIdea);
+            res.redirect("/ideas");
+            return null;
         }, new HandlebarsTemplateEngine());
     }
 }
